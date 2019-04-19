618b1195bcdbdbccaf9d0470426a70231a2a8f83
Szymon Polanowski <szympol@hotmail.com>
2019-04-19 19:01:11 +0200

    Fix bug of navigation - set menu from mobile to desktop view

--- js/script-compiled.js
+++ js/script-compiled.js
@@ -1,1 +1,62 @@
 'use strict';
+
+// variables
+
+var menu = document.querySelector('.hamburger');
+var navbarWrapper = document.getElementById('navbar-wrapper');
+var navbarSections = document.getElementById('navbar-sections');
+
+var classesNavbarWrapper = navbarWrapper.classList;
+
+var ifHasNavbarWrapperForMobileView = void 0;
+
+// toggle menu on mobile viewport
+menu.addEventListener(
+  'click',
+  function () {
+    this.classList.toggle('hamburger--active');
+  },
+  false
+);
+
+function toggleMenu () {
+  navbarWrapper.classList.toggle('navbar-wrapper--mobile');
+  navbarSections.classList.toggle('navbar-sections--mobile');
+  ifHasNavbarWrapperForMobileView = classesNavbarWrapper.contains(
+    'navbar-wrapper--mobile'
+  );
+}
+
+function removeMobileToggle () {
+  navbarWrapper.classList.remove('navbar-wrapper--mobile');
+  navbarSections.classList.remove('navbar-sections--mobile');
+  menu.classList.remove('hamburger--active');
+}
+
+menu.addEventListener('click', function (e) {
+  e.preventDefault();
+  toggleMenu();
+});
+
+// listening to resize
+var viewportWidth = void 0;
+
+var setViewportWidth = function setViewportWidth () {
+  viewportWidth = window.innerWidth;
+  // console.log(viewportWidth);
+};
+
+setViewportWidth();
+
+window.addEventListener(
+  'resize',
+  function () {
+    setViewportWidth();
+    if (viewportWidth > 768 && ifHasNavbarWrapperForMobileView) {
+      removeMobileToggle();
+    } else {
+      return false;
+    }
+  },
+  false
+);
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,61 @@
 'use strict';
+
+// variables
+const menu = document.querySelector('.hamburger');
+const navbarWrapper = document.getElementById('navbar-wrapper');
+const navbarSections = document.getElementById('navbar-sections');
+
+const classesNavbarWrapper = navbarWrapper.classList;
+
+let ifHasNavbarWrapperForMobileView;
+
+// toggle menu on mobile viewport
+menu.addEventListener(
+  'click',
+  function () {
+    this.classList.toggle('hamburger--active');
+  },
+  false
+);
+
+function toggleMenu () {
+  navbarWrapper.classList.toggle('navbar-wrapper--mobile');
+  navbarSections.classList.toggle('navbar-sections--mobile');
+  ifHasNavbarWrapperForMobileView = classesNavbarWrapper.contains(
+    'navbar-wrapper--mobile'
+  );
+}
+
+function removeMobileToggle () {
+  navbarWrapper.classList.remove('navbar-wrapper--mobile');
+  navbarSections.classList.remove('navbar-sections--mobile');
+  menu.classList.remove('hamburger--active');
+}
+
+menu.addEventListener('click', function (e) {
+  e.preventDefault();
+  toggleMenu();
+});
+
+// listening to resize
+let viewportWidth;
+
+let setViewportWidth = () => {
+  viewportWidth = window.innerWidth;
+  // console.log(viewportWidth);
+};
+
+setViewportWidth();
+
+window.addEventListener(
+  'resize',
+  function () {
+    setViewportWidth();
+    if (viewportWidth > 768 && ifHasNavbarWrapperForMobileView) {
+      removeMobileToggle();
+    } else {
+      return false;
+    }
+  },
+  false
+);
