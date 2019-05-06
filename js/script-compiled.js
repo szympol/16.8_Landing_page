2167ae7d6a5a1f7a7599834eb22230f694746447
Szymon Polanowski <szympol@hotmail.com>
2019-05-06 23:50:35 +0200

    Add map

--- js/script-compiled.js
+++ js/script-compiled.js
@@ -267,3 +267,120 @@ $(window).scroll(function () {
     a = 1;
   }
 });
+
+// google map
+
+window.initMap = function () {
+  var manchester = { lat: 53.470406, lng: -2.239299 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: manchester,
+    styles: [{
+      elementType: 'geometry',
+      stylers: [{
+        color: '#f5f5f5'
+      }]
+    }, {
+      elementType: 'labels.icon',
+      stylers: [{
+        visibility: 'off'
+      }]
+    }, {
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#616161'
+      }]
+    }, {
+      elementType: 'labels.text.stroke',
+      stylers: [{
+        color: '#f5f5f5'
+      }]
+    }, {
+      featureType: 'administrative.land_parcel',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#bdbdbd'
+      }]
+    }, {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#eeeeee'
+      }]
+    }, {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#757575'
+      }]
+    }, {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#e5e5e5'
+      }]
+    }, {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#9e9e9e'
+      }]
+    }, {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#ffffff'
+      }]
+    }, {
+      featureType: 'road.arterial',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#757575'
+      }]
+    }, {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#dadada'
+      }]
+    }, {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#616161'
+      }]
+    }, {
+      featureType: 'road.local',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#9e9e9e'
+      }]
+    }, {
+      featureType: 'transit.line',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#e5e5e5'
+      }]
+    }, {
+      featureType: 'transit.station',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#eeeeee'
+      }]
+    }, {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#c9c9c9'
+      }]
+    }, {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#9e9e9e'
+      }]
+    }]
+  });
+
+  var marker = new google.maps.Marker({ position: manchester, map: map });
+};
